423a4505cbb1a0d94040aa31d681a5a4c33d7508
Pu5hk4r <pushstack25@gmail.com>
2026-02-09 23:52:19 +0530

    String practice code added

--- POMODORO_CLOCK/Solution - pomodoro-end/main.py
+++ POMODORO_CLOCK/Solution - pomodoro-end/main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import math
+import os
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -75,7 +76,8 @@ title_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
 title_label.grid(column=1, row=0)
 
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
-tomato_img = PhotoImage(file="tomato.png")
+script_dir = os.path.dirname(os.path.abspath(__file__))
+tomato_img = PhotoImage(file=os.path.join(script_dir, "tomato.png"))
 canvas.create_image(100, 112, image=tomato_img)
 timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
